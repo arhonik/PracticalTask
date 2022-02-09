67a7e8ac245d1f5a4b78563a7d89fca34c1f7cf5
arhonik <kotnyaa@gmail.com>
2022-02-09 15:28:54 +0400

    IS-1 Add class Report

---
+++ app/Report.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+class Report
+{
+    public function __construct()
+    {
+    }
+
+    public function goToBodyReport()
+    {
+
+    }
+
+    public function goToHeadersReport()
+    {
+
+    }
+
+    public function getPointerPositionReport()
+    {
+
+    }
+
+    public function getLneReport()
+    {
+
+    }
+
+    private function goToNextLineReport()
+    {
+
+    }
+
+    public function __destruct()
+    {
+    }
+}
