617f8b9c2d5a6b62c8003487ff77a3fd64714bec
arhonik <kotnyaa@gmail.com>
2022-02-09 09:57:43 +0400

    IS-1 Add class ReportReader

---
+++ app/ReportReader.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+class ReportReader
+{
+    private string $fullPathToReport;
+    
+    public function __construct()
+    {
+    }
+
+    public function readReportTitle()
+    {
+        
+    }
+
+    public function readReportRecord()
+    {
+        
+    }
+
+    public function readReporttChank()
+    {
+        
+    }
+}
