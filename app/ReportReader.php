d61116bc241366f2765a027bdaef10bfd9d33f73
arhonik <kotnyaa@gmail.com>
2022-02-09 13:39:16 +0400

    IS-1 refactor method readReportTitle

--- app/ReportReader.php
+++ app/ReportReader.php
@@ -15,7 +15,7 @@ class ReportReader
 
     public function readReportTitle(): \App\ReportRecord
     {
-       $this->jumpToTheBeginningOfTheFile();
+        $this->jumpToTheBeginningOfTheFile();
 
         $reportRecord = new \App\ReportRecord();
         $data = fgetcsv($this->report, 0, ';');
