eb014bb1502cc7f413c1b6acb9c8da5e09601eb0
arhonik <kotnyaa@gmail.com>
2022-02-11 15:00:09 +0400

    IS-1 Update interface ReportLine

--- app/modules/ReportReader/ReportLineInterface.php
+++ app/modules/ReportReader/ReportLineInterface.php
@@ -4,31 +4,17 @@ namespace App\Modules\ReportReader;
 
 interface ReportLineInterface
 {
-    public function setId(string $id);
-
     public function getId(): string;
-
-    public function setCustomerName(string $customerName): void;
 
     public function getCustomerName(): string;
 
-    public function setProductName(string $productName): void;
-
     public function getProductName(): string;
-
-    public function setProductQuantity(string $productQuantity): void;
 
     public function getProductQuantity(): string;
 
-    public function setProductArticle(string $productArticle): void;
-
     public function getProductArticle(): string;
 
-    public function setProductWeight(string $productWeight): void;
-
     public function getProductWeight(): string;
-
-    public function setProductPrice(string $productPrice): void;
 
     public function getProductPrice(): string;
 
