5df405327f34a2f2b2ed5886f9aac7c2045710a3
arhonik <kotnyaa@gmail.com>
2022-02-11 15:37:24 +0400

    IS-1 Rename variable

--- script.php
+++ script.php
@@ -3,11 +3,11 @@
 require_once __DIR__.'/autoload.php';
 
 $fullPathToReport = __DIR__ . '/resource/report.csv';
-$reportCreator = new \App\Modules\ReportCreator\Report($fullPathToReport);
-$header = $reportCreator->getHeader();
-while ($data = $reportCreator->getRecord()) {
+$report = new \App\Modules\ReportCreator\Report($fullPathToReport);
+$header = $report->getHeader();
+while ($data = $report->getRecord()) {
     //process report data
 }
-while ($data = $reportCreator->getChunk(3)) {
+while ($data = $report->getChunk(3)) {
     //process report data
 }
