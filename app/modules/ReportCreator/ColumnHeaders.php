b86aa6e961b4d46126505243e6daf131ecb96c21
arhonik <kotnyaa@gmail.com>
2022-02-13 23:49:28 +0400

    IS-1 Add constructor ColumnHeaders

--- app/modules/ReportCreator/ColumnHeaders.php
+++ app/modules/ReportCreator/ColumnHeaders.php
@@ -14,6 +14,31 @@ class ColumnHeaders
 
     public function __construct(array $row)
     {
+        foreach ($row as $position => $title) {
+            switch ($title) {
+                case ColumnName::ID:
+                    $this->idInfo = new HeaderInfo($title, $position);
+                    break;
+                case ColumnName::CUSTOMER_NAME:
+                    $this->customerNameInfo = new HeaderInfo($title, $position);
+                    break;
+                case ColumnName::PRODUCT_NAME:
+                    $this->productNameInfo = new HeaderInfo($title, $position);
+                    break;
+                case ColumnName::PRODUCT_QUANTITY:
+                    $this->productQuantityInfo = new HeaderInfo($title, $position);
+                    break;
+                case ColumnName::PRODUCT_ARTICLE:
+                    $this->productArticleInfo = new HeaderInfo($title, $position);
+                    break;
+                case ColumnName::PRODUCT_WEIGHT:
+                    $this->productWeightInfo = new HeaderInfo($title, $position);
+                    break;
+                case ColumnName::PRODUCT_PRICE:
+                    $this->productPriceInfo = new HeaderInfo($title, $position);
+                    break;
+            }
+        }
     }
 
     public function getIdInfo(): HeaderInfo
