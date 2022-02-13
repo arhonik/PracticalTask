101cd9fe18c0a2263cf7611c5b1e946fb1bcd356
arhonik <kotnyaa@gmail.com>
2022-02-13 23:46:53 +0400

    IS-1 Add ColumnName

---
+++ app/modules/ReportCreator/ColumnName.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Modules\ReportCreator;
+
+class ColumnName
+{
+    const ID = 'ID';
+    const CUSTOMER_NAME = 'Фамилия';
+    const PRODUCT_NAME = 'Наименование';
+    const PRODUCT_QUANTITY = 'Количество';
+    const PRODUCT_ARTICLE = 'Артикул';
+    const PRODUCT_WEIGHT = 'Вес';
+    const PRODUCT_PRICE = 'Цена';
+}
